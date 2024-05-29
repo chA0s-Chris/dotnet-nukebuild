2156677ab7a54a9fbd149cf975ea7644514089e3
Christian Flessa <chris@z0rn.net>
2024-05-30 00:12:32 +0200

    WiP

--- build.sh
+++ build.sh
@@ -1,38 +1,80 @@
 #!/bin/bash
 #
+IMAGE_TEMP_PATH="./tmp"
+IMAGE_DOCKERFILE="${IMAGE_TEMP_PATH}/Dockerfile"
+FEATURES_PATH="./features"
 
-BUILD_IMAGE_NAME="chaos/dotnet-nukebuild"
-DOCKER_PLUGIN_PATH="${HOME}/.docker/cli-plugins"
-DOCKER_PUSHRM_CMD="${DOCKER_PLUGIN_PATH}/docker-pushrm"
-DOCKER_PUSHRM_RELEASE="https://github.com/christian-korneck/docker-pushrm/releases/download/v1.9.0/docker-pushrm_linux_amd64"
+get_docker_pushrm() {
+  mkdir -p ${DOCKER_PLUGIN_PATH}
+  curl -L ${DOCKER_PUSHRM_RELEASE} -o ${DOCKER_PUSHRM_CMD}
+  chmod +x ${DOCKER_PUSHRM_CMD}
+}
 
-mkdir -p ${DOCKER_PLUGIN_PATH}
-curl -L ${DOCKER_PUSHRM_RELEASE} -o ${DOCKER_PUSHRM_CMD}
-chmod +x ${DOCKER_PUSHRM_CMD}
+remove_temp_path() {
+  rm -rf "${IMAGE_TEMP_PATH}"
+}
 
-. ./versions
+create_temp_path() {
+  mkdir -p "${IMAGE_TEMP_PATH}"
+}
 
-docker build -t ${BUILD_IMAGE_NAME}:${IMAGE_RELEASE_VERSION} ./release
-docker tag ${BUILD_IMAGE_NAME}:${IMAGE_RELEASE_VERSION} ${BUILD_IMAGE_NAME}:latest
-docker tag ${BUILD_IMAGE_NAME}:${IMAGE_RELEASE_VERSION} ${BUILD_IMAGE_NAME}:${IMAGE_RELEASE_MAJOR}
+add_feature() {
+  if [ "${2}" = true ]; then
+    echo "Adding feature: ${1}"
 
-if [ -n "${IMAGE_PRERELEASE_VERSION}" ]; then
-    docker build -t ${BUILD_IMAGE_NAME}:${IMAGE_PRERELEASE_VERSION} ./pre
-    docker tag ${BUILD_IMAGE_NAME}:${IMAGE_PRERELEASE_VERSION} ${BUILD_IMAGE_NAME}:preview
-else
-    docker tag ${BUILD_IMAGE_NAME}:${IMAGE_RELEASE_VERSION} ${BUILD_IMAGE_NAME}:preview
-fi
+    cat "${FEATURES_PATH}/${1}" >> "${IMAGE_DOCKERFILE}"
+  fi
+}
 
-echo ${CI_DOCKER_TOKEN} | docker login -u ${CI_DOCKER_LOGIN} --password-stdin
+create_image() {
+  echo "Using base image file: ${BASE_IMAGE_FILE}"
 
-docker push ${BUILD_IMAGE_NAME}:${IMAGE_RELEASE_VERSION}
-docker push ${BUILD_IMAGE_NAME}:latest
-docker push ${BUILD_IMAGE_NAME}:${IMAGE_RELEASE_MAJOR}
+  # copy base
+  cp "${BASE_IMAGE_FILE}" "${IMAGE_DOCKERFILE}"
 
-if [ -n "${IMAGE_PRERELEASE_VERSION}" ]; then
-    docker push ${BUILD_IMAGE_NAME}:${IMAGE_PRERELEASE_VERSION}
-fi
+  add_feature "set_environment" "${FEATURE_SET_ENVIRONMENT}"
+  add_feature "update_distro" "${FEATURE_UPDATE_DISTRO}"
+  add_feature "install_docker" "${FEATURE_INSTALL_DOCKER}"
+  add_feature "install_git-lfs" "${FEATURE_INSTALL_GIT_LFS}"
+  add_feature "install_azure_devops_provider" "${FEATURE_INSTALL_AZURE_DEVOPS_PROVIDER}"
+  add_feature "install_docker_pushrm" "${FEATURE_INSTALL_DOCKER_PUSHRM}"
+  add_feature "install_kubectl" "${FEATURE_INSTALL_KUBECTL}"
+  add_feature "install_nuke" "${FEATURE_INSTALL_NUKE}"
+}
 
-docker push ${BUILD_IMAGE_NAME}:preview
+create_tag_parameters() {
+  TAG_PARAMETERS=""
 
-docker pushrm ${BUILD_IMAGE_NAME}
+  for tag in ${IMAGE_TAGS[@]}; do
+    TAG_PARAMETERS="${TAG_PARAMETERS} -t ${BUILD_IMAGE_NAME}:${tag}"
+  done
+}
+
+build_image() {
+  create_tag_parameters
+  
+  echo "Building image..."
+  
+  docker build ${TAG_PARAMETERS} -f ${IMAGE_DOCKERFILE} .
+}
+
+# include defaults
+. ./defaults
+
+remove_temp_path
+create_temp_path
+
+#get_docker_pushrm
+
+for image in $(find ./images -type f); do
+  echo "Image found: ${image}"
+
+  # include image configuration
+  . ${image}
+
+  create_image
+  build_image
+done
+
+#remove_temp_path
+
